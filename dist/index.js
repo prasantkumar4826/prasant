f70389384b25196484f07198da90d6512fe001b0
prasant kumar <prasantkumar4826@gmail.com>
2021-08-11 12:48:03 +0530

    add

--- dist/index.js
+++ dist/index.js
@@ -6287,19 +6287,20 @@ const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
-    await octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-      });
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+  await octokit.rest.issues.createComment({
+  ...context.repo,
+  issue_number: pull_request.number,
+  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+});
   }
   
   run();
+
 })();
 
 module.exports = __webpack_exports__;
