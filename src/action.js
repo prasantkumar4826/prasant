ea2a8fdd6d2a939da94a4e8aac47ed9a8774918c
prasant kumar <prasantkumar4826@gmail.com>
2021-08-11 17:11:42 +0530

    Update action.js

--- src/action.js
+++ src/action.js
@@ -10,7 +10,7 @@ async function run() {
   await octokit.rest.issues.createComment({
   ...context.repo,
   issue_number: pull_request.number,
-  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.hiiiii'
+  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can  huiiiiiii.'
 });
   }
   
