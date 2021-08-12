f88d3c019b5d590c65d37c94e28ee80a1b8faeb5
prasant kumar <prasantkumar4826@gmail.com>
2021-08-12 19:10:55 +0530

    add

--- dist/index.js
+++ dist/index.js
@@ -6254,7 +6254,11 @@ async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-
+  const randomPos = Math.round(Math.random() * 1000);
+  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+  const response = await fetch(url);
+  const { results } = await response.json();
+  const gifUrl = results[0].media[0].tinygif.url;
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
