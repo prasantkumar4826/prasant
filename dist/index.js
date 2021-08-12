9baf1fa5467da3addfe5f758b5b5233ebdcd90fd
prasant kumar <prasantkumar4826@gmail.com>
2021-08-12 15:26:24 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6267,7 +6267,7 @@ async function run() {
   await octokit.rest.issues.createComment({
   ...context.repo,
   issue_number: pull_request.number,
-  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<table><tr><th>Month</th><th>Savings</th></tr><tr><td>January</td><td>$100</td></tr></table>  <iframe herf="https://artisanbazaar.org" ></iframe>'
+  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<table><tr><th>Month</th><th>Savings</th></tr><tr><td>January</td><td>$100</td></tr></table>  <iframe href="https://artisanbazaar.org" ></iframe>'
 });
   }
   
